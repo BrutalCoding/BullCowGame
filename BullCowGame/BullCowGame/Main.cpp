7a3b20fa1145e3ed3ca8eb014e6a6d342116939c
Daniel Breedeveld <dhbreedeveld@gmail.com>
2017-08-12 15:54:31 +0200

    Added std namespace and simple welcome message and task.

---
+++ BullCowGame/BullCowGame/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	constexpr int WORLD_LENGTH = 6;
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I'm thinking off?" << endl;
+	return 0;
+}
