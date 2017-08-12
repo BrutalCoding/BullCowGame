0b551b57669bc79788b5760153d7b6f9d72418bd
Daniel Breedeveld <dhbreedeveld@gmail.com>
2017-08-12 17:24:23 +0200

    Replaced cin with getline() so we can read out the entire line (else it would stop at the first white space)

--- BullCowGame/BullCowGame/Main.cpp
+++ BullCowGame/BullCowGame/Main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	constexpr int WORLD_LENGTH = 6;
+	//Introduce the game
+	constexpr int WORLD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows" << endl;
 	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I'm thinking off?" << endl;
+
+	//Get a guess from the player
+	string Guess = "";
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+
+	//Repeat the guess back to the player
+	cout << "You've guessed: " << Guess << endl;
+
 	return 0;
 }
